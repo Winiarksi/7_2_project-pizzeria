f3c096fb553d5c48ddd6fa339d99b4caf9866cd6
Artur Winiarski <winiarski.a.r@gmail.com>
2020-09-21 21:37:20 +0200

    add initData

--- src/js/script.js
+++ src/js/script.js
@@ -65,6 +65,10 @@
       const testProduct = new Product();
       console.log('testProduct: ', testProduct);
     },
+    initData: function () {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
     init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
@@ -73,6 +77,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
