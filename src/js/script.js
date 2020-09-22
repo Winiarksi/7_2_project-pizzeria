cea651573d1ce15916ff6beb21e08d252d368935
Artur Winiarski <winiarski.a.r@gmail.com>
2020-09-22 22:08:28 +0200

    new method initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -59,11 +59,12 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product: ', thisProduct);
     }
 
-    renderInMenu(){
+    renderInMenu() {
       const thisProduct = this;
 
       const generatedHTML = templates.menuProduct(thisProduct.data);
@@ -74,7 +75,34 @@
 
       menuContainer.appendChild(thisProduct.element);
     }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      console.log(thisProduct.element.clickable);
+      /* START: click event listener to trigger */
+
+      /* prevent default action for event */
+
+      /* toggle active class on element of thisProduct */
+
+      /* find all active products */
+
+      /* START LOOP: for each active product */
+
+      /* START: if the active product isn't the element of thisProduct */
+
+      /* remove class active for the active product */
+
+      /* END: if the active product isn't the element of thisProduct */
+
+      /* END LOOP: for each active product */
+
+      /* END: click event listener to trigger */
+    }
   }
+
 
   const app = {
     initMenu: function () {
